5586decec13dddae95efb373ea9c044e83a20d2e
Simon Wessel <simon.w.karlsson@gmail.com>
2014-09-14 22:08:07 +0200

    Add exercise 2-4 of chapter 2.

--- include/ch2.hpp
+++ include/ch2.hpp
@@ -2,5 +2,8 @@
 #define CH2_HPP
 
 int ch2ex1();
+int ch2ex2();
+int ch2ex3(int current_year, int car_year);
+int ch2ex4(double miles_per_gallon);
 
 #endif // CH2_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,10 @@ using namespace std;
 int main()
 {
     //ch1ex2();
-    ch2ex1();
+    //ch2ex1();
+    //ch2ex2();
+    //ch2ex3(2025, 2000);
+    ch2ex4(5.5);
 
     return 0;
 }
