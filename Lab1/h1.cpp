092ae8fd8ef2b00075b549237cf12c233ca4c932
Simon Wessel <simon.w.karlsson@gmail.com>
2014-11-07 14:37:18 +0100

    Add comments

--- Lab1/h1.cpp
+++ Lab1/h1.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 int comp (int, int, int);
+int comp2 (int, int, int);
 
 int main() {
-    // x = (a > b) ? a : (b > c) ? b : c ; as if statements
 
+    cout << "Using comp" << endl;  
     for (int a = 1; a <= 3; a++) {
         for (int b = 1; b <= 3; b++) {
             for (int c = 1; c <= 3; c++) {
@@ -21,18 +22,46 @@ int main() {
             }
         }
     }
+    cout << "--------------------------" << endl;
+
+    cout << "Using comp2" << endl;
+    for (int a = 1; a <= 3; a++) {
+        for (int b = 1; b <= 3; b++) {
+            for (int c = 1; c <= 3; c++) {
+                int x  = 0;
+                int m = a; if (b > m) m = b; if (c > m) m = c;
+
+                x = comp2 (a, b, c);
+
+                cout << "a b c x = " << a << " " << b << " " << c << " " << x;
+                if (x != m) cout << " Fel: max = " << m;
+                cout << endl;
+            }
+        }
+    }
 
     return 0;
 }
 
+// Literal translation of x = (a > b) ? a : (b > c) ? b : c;
+// Generates wrong maximum for comp(2, 1, 3)
 int comp (int a, int b, int c) {
-    if (a >= b && a >= c) {
+    if (a > b) {
         return a;
-    } else if (b >= a && b >= c) {
+    } else if (b > c) {
         return b;
     } else {
         return c;
     }
 }
 
-
+// Fixed version
+int comp2 (int a, int b, int c) {
+    if (a > b && a > c) {
+        return a;
+    } else if (b > c) {
+        return b;
+    } else {
+        return c;
+    }
+}
