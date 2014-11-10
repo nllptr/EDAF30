0a1549823e79fcba4c62e9df199ce6ded8d02a2c
Gustav Jansson Ekstrand <gustav.jp@live.se>
2014-11-10 13:48:26 +0100

    Cleaned up, fixed some bugs and added U9

--- Lab1/U9-2.cpp
+++ Lab1/U9-2.cpp
@@ -6,25 +6,18 @@ using namespace std;
 string toUpperCase(string);
 
 int main() {
+    string alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";
     string in;
     string out = "";
-    cout << "Skriv ett meddelande (A-Z): ";
     getline(cin, in);
-    if(in.length() > 80) {
-        cout << "Meddelande för långt.";
-        return 1;
-    }
     in = toUpperCase(in);
     for(int i = 0; i < in.length(); i++) {
         if(in[i] >= 'A' && in[i] <= 'Z') {
-            if(in[i] + 13 > 90) {
-                out +=(in[i] + 13) % 90 + 64;
-            } else {
-                out += in[i] + 13;
-            }
+            out += alphabet[in[i]%65];
         } else {
             out += in[i];
         }
+
     }
     cout << out;
 }
