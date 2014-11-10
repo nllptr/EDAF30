eca1f64fd6a404559cfc94a94ea7d39182f5d78e
Gustav Jansson Ekstrand <gustav.jp@live.se>
2014-11-10 12:55:09 +0100

    Assignment U10 done

--- Lab1/U10-2.cpp
+++ Lab1/U10-2.cpp
@@ -8,7 +8,7 @@ int main() {
     cout << "Skriv in romerskt tal: ";
     cin >> roman;
     int decimal[roman.length()];
-    int total;
+    int total = 0;
 
     for(int i = 0; i < roman.length(); i++) {
         switch(roman[i]) {
@@ -19,16 +19,34 @@ int main() {
         case 'C': decimal[i] = 100; break;
         case 'D': decimal[i] = 500; break;
         case 'M': decimal[i] = 1000; break;
-        default: decimal[i] = 0; break;
+        default: cout << roman[i] << " is not a valid number." << endl; return 1;
         }
     }
-    for(int i = 0; i < roman.length(); i++) {
-        cout << decimal[i] << endl;
-    }
 
     for(int i = 0; i < roman.length(); i++) {
-
+        if(i < roman.length()) {
+            if(decimal[i] == decimal[i+1]/2) {
+                cout << "Invalid format. " << roman[i] << " cannot preceed " << roman[i+1] << endl;
+                return 1;
+            } else if (i < roman.length() - 1 && decimal[i] < decimal[i+2]) {
+                cout << "The sequence " << roman[i] << roman[i+1] << roman[i+2] << " is not allowed." << endl;
+                return 1;
+            }
+        }
     }
 
-
+    if(roman.length() > 1) {
+        for(int i = 0; i < roman.length(); i++) {
+            if(decimal[i] > decimal[i+1] && i+1< roman.length()) {
+                total += decimal[i];
+            } else if (decimal[i] < decimal[i+1] && i+1 < roman.length()){
+                total -= decimal[i];
+            } else {
+                total += decimal[i];
+            }
+        }
+    } else {
+        total = decimal[0];
+    }
+    cout << roman << " = " << total;
 }
