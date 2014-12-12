2b562b7dd82ea030cf8c25389d69270532d16452
Simon Wessel <simon.w.karlsson@gmail.com>
2014-12-12 23:13:40 +0100

    Added another test to U2

--- Lab6/U2-1/main.cpp
+++ Lab6/U2-1/main.cpp
@@ -7,10 +7,16 @@ using namespace std;
 
 int main()
 {
+    cout << "Test 1:" << endl;
     int test1[] = {5, 22, 11, 7, 9, 2, 7, 3, 1, 32, 6};
     skrivUtFalt(test1, 10);
     quicksort::quicksort(test1, 0, 9);
     skrivUtFalt(test1, 10);
+    cout << endl << endl << "Test 2:" << endl;
+    char str[] = "Testar lite med tecken";
+    skrivUtFalt(str, 22);
+    quicksort::quicksort(str, 0, 21);
+    skrivUtFalt(str, 22);
     return 0;
 }
 
