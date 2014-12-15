836f62bd0abbb399eb557cb54edbd66886c794fd
Gustav Jansson Ekstrand <gustav.jp@live.se>
2014-12-15 15:47:55 +0100

    U7 done

--- Lab6/U7-2/main.cpp
+++ Lab6/U7-2/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include "dates.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int year = 92;
+    int month = 1;
+    int day = 19;
+    short int packed = pack(year, month, day);
+    int y;
+    int m;
+    int d;
+    unpack(packed, y, m, d);
+    cout << "y: " << y << endl;
+    cout << "m: " << m << endl;
+    cout << "d: " << d << endl;
+
+    cout << hex << endl << packed << endl;
     return 0;
 }
